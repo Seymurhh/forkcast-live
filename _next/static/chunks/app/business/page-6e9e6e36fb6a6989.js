011e6c317f747f44873c945b534162cc064bbb56
Seymur Hasanov <shasanov@seas.harvard.edu>
2026-06-28 21:47:30 -0400

    Forkcast static site (compiled app only)

---
+++ _next/static/chunks/app/business/page-6e9e6e36fb6a6989.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[376,809],{99827:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,52619,23))}},e=>{e.O(0,[619,441,493,358],()=>e(e.s=99827)),_N_E=e.O()}]);
